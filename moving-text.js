1f89b0a8201e2362d0c62577adf5fa4517a72a28
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-18 22:04:37 -0800

    Update moving-text.js

--- moving-text.js
+++ moving-text.js
@@ -1,25 +1,27 @@
+
 // Where is the text
 var x, y;
 
 function setup() {
-  createCanvas(720, 400);
+  createCanvas(400, 400);
   // Starts in the middle
   x = width
   y = height/2;
 }
 
 function draw() {
-  background(200);
+  background(183, 211, 242);
   
   // Notes to preferred song
   fill(100);
-  text('ccaag',x, y);
+	textSize(32);
+  text('ccggaag ffeeddc ggffeed ggffeed ccggaag ffeeddc',x, y);
   
   // Moving left at a constant speed
   x = x - 1;
   
   // Reset to the right side of the canvas
-  if (x < 0) {
+  if (x < -800) {
     x = width;
   }
 }
