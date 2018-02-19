af3c9ecb635d28fd96d7f41eb2b1a8001994f7bc
chelseycampillo <35706456+chelseycampillo@users.noreply.github.com>
2018-02-18 17:54:45 -0800

    Update smoke-modified.js

--- smoke-modified.js
+++ smoke-modified.js
@@ -1,42 +1,40 @@
-//rainbow of space ships
-
-var x = [];
+var x = 230;
 var y = [];
-var colors = [];
-var r = 0;
+var ctr = 0;
+var ySpeed = [];
 
 function setup() {
   createCanvas(400, 400);
-	for (var i = 0; i<20; i = i + 0.5) {
-		x[i] = width / 2;
-		y[i] = height / 2;
-		colors[i] = color(random(255), random(255), random(255));
+  colorMode(HSB)
+	
+	for (var i =0; i < 100; i = i + 1) {
+		ySpeed[i] = random(0,height);
 	}
 }
   
 function draw() {
   background(0);
   noStroke();
-	
-	for (var i = 0; i < 20; i = i + 0.5) {
-		fill(colors[i]);
-		translate(x,y);
-		rotate(r);
-		ellipse (x[i], y[i], 60, 60);
-		ellipse (x[i], y[i], 40, 80);
 
-		y[i] = y[i] + 2;
+  // draw pipe
+  rect(0, 200, x, 20);
   
-		// rotate 0.05 radians ~= 2.8 degrees per frame
-  	r += 0.3
-  
-  // if reach past the top a bunch
-		if (x[i] > 500) {
-			x[i] = 0;
-		}
-			
-  	if (y[i] > 500) {
-    	y[i] = 0;
-  }
+  // droplets
+	for (var i = 0; i <= ctr; i = i + 1) {
+		ellipse(x, y[i], 10);
 	}
+  
+	
+	//randomly decide if new ball should come
+	if (random(0, 10) < 2) {
+		ctr += 1;
+		y[ctr] = 220;
+		ySpeed[ctr] = random(1, 10);
+	}
+  
+  // down 3 pixels each frame, but maybe should be accelerating?
+  for (var i = 0; i < ctr; i++) {
+		y[i] = y[i] + ySpeed[i];
+	}
+	
 }
